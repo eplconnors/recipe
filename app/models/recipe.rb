bd2a2285b19ce92598eb8d1a7321361857c87b6d
Tyler Seawell <tylerseawell4@gmail.com>
2016-04-21 14:31:33 +0000

    forms added/fixed some avatar stuff

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,7 +1,7 @@
 class Recipe < ActiveRecord::Base
+    has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png" 
+    validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
     belongs_to :nationality
     belongs_to :diet
 
-has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png" validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
->>>>>>> gemedit
 end
